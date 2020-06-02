b8d4069a2f76a618db3a9891486f5fa6f598117a
Jim Hough <teachlife@outlook.com>
2020-06-02 14:22:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(rootNode, vertices, edges){
+  let vert = []
+  for (edge in edges){
+    if (edge.includes(rootNode)){
+      for (vertice in edge){
+        if(vertice !== rootNode){
+          let connection = vertices.find(e => e.name = vertice)
+          vert.push(connection)
+        }
+      }
+    }
+  }
+  return vert
+}
