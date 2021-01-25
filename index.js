43cd8b3a519abb8be163c7ffd529495c66f4cc1c
NickSpangler <Spangleyman@mac.com>
2021-01-25 14:06:35 -0500

    solve findAdjacent()

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(nodeName,  vertices, edges){
+    let adjacentEdges = edges.filter(edge => {
+        return edge.includes(nodeName)
+    })
+    let adjacentNames = adjacentEdges.map(edge => edge.filter(side => side !== nodeName)[0])
+    let adjacentNodes = adjacentNames.map(name => {
+        return vertices.filter(vertex => vertex.name === name)[0]
+    })
+    // .map(edge => edge.filter(el => el !== nodeName))
+    return adjacentNodes;
+  }
+
+function markDistanceAndPredecessor() {
+
+}
+
+// function pseudocode() {
+//     rootNode = vertices[0]
+//     queue = []
+//     addVertexToQueue(rootNode)
+//     while(queue.length !== 0) {
+//         let firstNode = queue.shift()
+//         let adjacentVertices = findAdjacent(firstNode)
+//         for (vertex in adjacentVertices) {
+//             markDistanceAndPredecessor(vertex)
+//             addToQueue(vertex)
+//         }
+//     }
+// }
